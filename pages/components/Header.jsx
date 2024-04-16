ec4a8cae916270103d5bde4be65d5863b6720d5f
Muruguganesan A <muruguganesan@cloudrevelinnovation.com>
2024-04-16 19:49:49 +0530

    new commit

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -1,8 +1,13 @@
+import Image from 'next/image';
+
 export default function Header() {
 
     return (
         <div className="header">
-            <img src="https://res.cloudinary.com/djdqpnyx4/image/upload/v1713162007/checklist_czat77.png" className="header_logo" alt="logo" />
+            {/* <img src="https://res.cloudinary.com/djdqpnyx4/image/upload/v1713162007/checklist_czat77.png" className="header_logo" alt="logo" /> */}
+            <Image src="https://res.cloudinary.com/djdqpnyx4/image/upload/v1713162007/checklist_czat77.png" className="header_logo" alt="logo" height={35} width={35} />
+
+            
             <span className="header_title">TODO</span>
         </div>
     )
--- pages/components/TodoItems.jsx
+++ pages/components/TodoItems.jsx
@@ -1,5 +1,6 @@
 import 'react-tooltip/dist/react-tooltip.css'
 import { Tooltip } from 'react-tooltip'
+import Image from 'next/image';
 
 export default function TodoItem({todoJSON,setTodoJSON}) {
     const handleCheckedFn = (k)=> {
@@ -29,11 +30,14 @@ export default function TodoItem({todoJSON,setTodoJSON}) {
                     </div>
                     <div className="todo_item_rgt">
                         <button className="edit_btn modifier_btn" data-tooltip-id="edit-tooltip" data-tooltip-content="Edit" data-tooltip-place="top">
-                            <img src="https://res.cloudinary.com/djdqpnyx4/image/upload/v1713192017/edit_s9tss9.svg" alt="edit" className="edit_img edit_delete_im" />
+                            {/* <img src="https://res.cloudinary.com/djdqpnyx4/image/upload/v1713192017/edit_s9tss9.svg" alt="edit" className="edit_img edit_delete_im" /> */}
+                            <Image src="https://res.cloudinary.com/djdqpnyx4/image/upload/v1713192017/edit_s9tss9.svg" alt="edit" className="edit_img edit_delete_im" height={30} width={30} />
+
                         </button>
                         <Tooltip id="edit-tooltip" />
                         <button className="delete_btn modifier_btn" onClick={()=>handleDeleteFn(ele.id)}  data-tooltip-id="delete-tooltip" data-tooltip-content="Delete" data-tooltip-place="top">
-                            <img src="https://res.cloudinary.com/djdqpnyx4/image/upload/v1713192018/delete_h5san6.svg" alt="delete" className="delete_img edit_delete_im"  />
+                            {/* <img src="https://res.cloudinary.com/djdqpnyx4/image/upload/v1713192018/delete_h5san6.svg" alt="delete" className="delete_img edit_delete_im"  /> */}
+                           <Image src="https://res.cloudinary.com/djdqpnyx4/image/upload/v1713192018/delete_h5san6.svg"  alt="delete" className="delete_img edit_delete_im"  height={30} width={30}  />
                         </button>
                         <Tooltip id="delete-tooltip" />
                     </div>
